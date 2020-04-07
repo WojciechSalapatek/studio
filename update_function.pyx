3a89ea8959950994293e3ce67402e262684b30b4
Aleksandra Kasznia <aleksandra@oc2440710718.ibm.com>
2020-04-07 19:49:48 +0200

    Fixes currents and adds var for possible future current correction

--- update_function.pyx
+++ update_function.pyx
@@ -10,14 +10,17 @@ def update_grid(np.ndarray[np.float64_t, ndim=2] input_grid, np.ndarray[np.float
         size_t j
         np.float64_t w
         np.float64_t n
+        np.gloat64_t wn
 
     for i in range(1, input_grid.shape[0]-1):
         for j in range(1, input_grid.shape[1]-1):
             n = ((north_currents_grid[i][j] + north_currents_grid[i][j+1])/2/north_max_current)
             w = ((west_currents_grid[i][j] + west_currents_grid[i-1][j])/2/west_max_current)
+            wn = sqrt(n*n + w*w)
+
             output_grid[i][j] = \
                 input_grid[i][j] \
-                + m*(((1+w)*input_grid[i-1][j] - (1-w)*input_grid[i][j]) + (input_grid[i+1][j] - input_grid[i][j]) + (input_grid[i][j-1] - input_grid[i][j]) + ((n+1)*input_grid[i][j+1]) - (n-1)*input_grid[i][j]) \
+                + m*(((1+w)*input_grid[i-1][j] - (1-w)*input_grid[i][j]) + (input_grid[i+1][j] - input_grid[i][j]) + ((n+1)*input_grid[i][j-1] - (n-1)*input_grid[i][j]) + (input_grid[i][j+1]) - input_grid[i][j]) \
                 + m*d*((input_grid[i-1][j-1] - input_grid[i][j]) + (input_grid[i-1][j+1] - input_grid[i][j]) + (input_grid[i+1][j-1] - input_grid[i][j]) + (input_grid[i+1][j+1] - input_grid[i][j]))
 
     return output_grid
