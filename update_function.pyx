f3bda0eb4f47f88d2f55c681a537f8b140eb99c3
WojciechSalapatek <wojciech.salapatek@gmail.com>
2020-04-16 17:52:49 +0200

    currents fix?

--- update_function.pyx
+++ update_function.pyx
@@ -29,7 +29,7 @@ def update_grid(np.ndarray[np.float64_t, ndim=2] input_grid, np.ndarray[np.float
 
             output_grid[i,j] = \
                 input_grid[i,j] \
-                + m*(((1+w)*input_grid[i-1,j] - (1-w)*input_grid[i,j]) + (input_grid[i+1,j] - input_grid[i,j]) + ((n+1)*input_grid[i,j-1] - (n-1)*input_grid[i,j]) + (input_grid[i,j+1]) - input_grid[i,j]) \
+                + m*(((1+n)*input_grid[i-1,j] - input_grid[i,j]) + ((1-n)*input_grid[i+1,j] - input_grid[i,j]) + ((1+w)*input_grid[i,j-1] - input_grid[i,j]) + ((1-w)*input_grid[i,j+1]) - input_grid[i,j]) \
                 + m*d*((input_grid[i-1,j-1] - input_grid[i,j]) + (input_grid[i-1,j+1] - input_grid[i,j]) + (input_grid[i+1,j-1] - input_grid[i,j]) + (input_grid[i+1,j+1] - input_grid[i,j]))
 
     return output_grid
